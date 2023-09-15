dad4c838f921f00a0cbc3324d318f9751755701c
yegormi <egormiropoltsev79@gmail.com>
2023-09-15 22:14:36 +0300

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Yegor Myropoltsev on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
